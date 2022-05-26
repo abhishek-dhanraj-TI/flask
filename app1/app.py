99efd51f30df83f20258580af1e081c5437f1804
abhishek-dhanraj-TI <abhishek.dhanraj@trilogy.com>
2022-05-26 10:34:23 +0000

    Initial commit

--- app1/app.py
+++ app1/app.py
@@ -11,7 +11,7 @@ def hello_world():
 @app.route('/widgets')
 def get_widgets():
   mydb = mysql.connector.connect(
-    host="mysqldb",
+    host="localhost",
     user="root",
     password="p@ssw0rd1",
     database="inventory"
@@ -35,7 +35,7 @@ def get_widgets():
 @app.route('/add')
 def add_items():
   mydb = mysql.connector.connect(
-    host="mysqldb",
+    host="localhost",
     user="root",
     password="p@ssw0rd1",
     database="inventory"
@@ -52,7 +52,7 @@ def add_items():
 @app.route('/initdb')
 def db_init():
   mydb = mysql.connector.connect(
-    host="mysqldb",
+    host="localhost",
     user="root",
     password="p@ssw0rd1"
   )
@@ -63,7 +63,7 @@ def db_init():
   cursor.close()
 
   mydb = mysql.connector.connect(
-    host="mysqldb",
+    host="localhost",
     user="root",
     password="p@ssw0rd1",
     database="inventory"
